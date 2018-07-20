272e89509bddbed0e6bc2c9c736e407e8065ba24
jayagl <jagrawal@isw.net.au>
2018-07-20 18:31:40 +1000

    color fix

--- config.js
+++ config.js
@@ -28,8 +28,8 @@ module.exports = {
   },
   start: {
     nodemon: (port) => `nodemon -w src --exec babel-node --inspect=${port} src`,
-    shared: () => `concurrently -p name -n USER,PROV,NOTF,LIC -c white.bgGreen,white.bgMagenta,black.bgCyan,black.bgWhite --kill-others "${START} user" "${START} provider" "${START} notification" "${START} licence"`,
-    all: () => `concurrently -p name -n CORE,APP,WEB,USER,PROV,NOTF,LIC -c white.bgRed,white.bgBlue,white.bgYellow,white.bgGreen,white.bgMagenta,black.bgCyan,black.bgWhite --kill-others "${START}" "${START} app" "${START} web" "${START} user" "${START} provider" "${START} notification" "${START} licence"`,
+    shared: () => `concurrently -p name -n USER,PROV,NOTF,LIC -c white.bgGreen,white.bgMagenta,black.bgCyan,black.bgWhite --kill-others "${START} user" "${START} provider" "${START} notification" "${START} licence" --color always`,
+    all: () => `concurrently -p name -n CORE,APP,WEB,USER,PROV,NOTF,LIC -c white.bgRed,white.bgBlue,white.bgYellow,white.bgGreen,white.bgMagenta,black.bgCyan,black.bgWhite --kill-others "${START}" "${START} app" "${START} web" "${START} user" "${START} provider" "${START} notification" "${START} licence" --color always`,
     dir: (dirName, web) => `cd ${dirName} && ${web ? WEBSTART : START}`,
   },
   ports: {
